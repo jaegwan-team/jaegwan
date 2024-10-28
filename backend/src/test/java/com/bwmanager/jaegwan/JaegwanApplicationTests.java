62e92a183e2cb5a6e46c3b1505e34b307adaf943
박경림 <pkl4693@naver.com>
2024-10-28 10:32:26 +0900

    init: backend project

---
+++ backend/src/test/java/com/bwmanager/jaegwan/JaegwanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bwmanager.jaegwan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaegwanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
