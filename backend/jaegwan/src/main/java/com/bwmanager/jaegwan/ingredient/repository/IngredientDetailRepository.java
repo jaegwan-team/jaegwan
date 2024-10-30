ea5d9d250828600750764fa2798b24c81dd2e858
ktk3939 <rkdxorud3939@naver.com>
2024-10-30 12:46:56 +0900

    feat: #10 재료 상세 Repository 생성

---
+++ backend/jaegwan/src/main/java/com/bwmanager/jaegwan/ingredient/repository/IngredientDetailRepository.java
@@ -0,0 +1,8 @@
+package com.bwmanager.jaegwan.ingredient.repository;
+
+import com.bwmanager.jaegwan.ingredient.entity.Ingredient;
+import com.bwmanager.jaegwan.ingredient.entity.IngredientDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IngredientDetailRepository extends JpaRepository<IngredientDetail, Long> {
+}
