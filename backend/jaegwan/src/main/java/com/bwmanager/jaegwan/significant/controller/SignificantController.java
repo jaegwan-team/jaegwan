7188ac7ef66667a741a73743f8a2da35d1897284
박세건 <qkrtprjs456@naver.com>
2024-10-30 10:56:56 +0900

    feat: SignificantController Controller 생성

---
+++ backend/jaegwan/src/main/java/com/bwmanager/jaegwan/significant/controller/SignificantController.java
@@ -0,0 +1,15 @@
+package com.bwmanager.jaegwan.significant.controller;
+
+
+import com.bwmanager.jaegwan.significant.service.SignificantService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/significant")
+public class SignificantController {
+
+    private SignificantService significantService;
+}
