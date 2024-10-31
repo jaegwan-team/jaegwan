69682cd5a854256d17e494ad7ac8945f1c86ed1c
박경림 <pkl4693@naver.com>
2024-10-31 10:52:57 +0900

    feat: #12 s3 util

--- backend/jaegwan/src/main/java/com/bwmanager/jaegwan/global/util/S3Util.java
+++ backend/jaegwan/src/main/java/com/bwmanager/jaegwan/global/util/S3Util.java
@@ -15,7 +15,8 @@ import software.amazon.awssdk.services.s3.model.S3Exception;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.concurrent.CompletableFuture;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @Service
@@ -26,30 +27,44 @@ public class S3Util {
     @Value("${cloud.aws.s3.bucket}")
     private String bucketName;
 
-    @Value("${cloud.aws.s3.prefix}")
-    private String prefix;
-
-    @Async
-    public CompletableFuture<String> upload(MultipartFile multipartFile, String filename) throws IOException {
-        // 원래 파일 이름에서 확장자를 추출합니다.
+    /**
+     * 외부에서 쓰는 메서드
+     * 파일, 접두어, id
+     *
+     * @param multipartFile
+     * @param prefix
+     * @param id
+     * @return
+     * @throws IOException
+     */
+    public String upload(MultipartFile multipartFile, String prefix, Long id) throws IOException {
+        // 원래 파일 이름에서 확장자를 추출한다.
         String originalFilename = multipartFile.getOriginalFilename();
-        String extension = "";
-        if (originalFilename != null && originalFilename.contains(".")) {
-            extension = originalFilename.substring(originalFilename.lastIndexOf("."));
-        }
 
-        // 새 파일 이름에 확장자를 추가합니다.
-        String key = prefix + "/" + filename + extension;
+        String key = generateKey(prefix, id) + originalFilename;
         try {
             putS3(multipartFile, key);
-            return CompletableFuture.completedFuture(findUploadKeyUrl(key));
+            return findUploadKeyUrl(key);
         } catch (S3Exception e) {
             log.error("Error uploading file to S3: {}", e.getMessage());
             throw e;
         }
     }
 
-    // 실제 업로드 하는 메소드
+    private String generateKey(String prefix, Long id) {
+        // 새 파일 이름에 오늘 날짜를 추가한다.
+        String date = LocalDate.now().format(DateTimeFormatter.ofPattern("yyMMdd"));
+        return prefix + "/" + id + "/" + date + "-";
+    }
+
+    /**
+     * 실제 업로드 하는 메소드
+     *
+     * @param file
+     * @param key
+     * @return
+     * @throws IOException
+     */
     private String putS3(MultipartFile file, String key) throws IOException {
         PutObjectRequest objectRequest = getPutObjectRequest(key, file.getContentType());
         RequestBody rb = getFileRequestBody(file);
@@ -57,24 +72,39 @@ public class S3Util {
         return findUploadKeyUrl(key);
     }
 
-    // 파일 업로드를 하기위한 PutObjectRequest를 반환합니다.
-    // 주의 사항은 com.amazonaws Package가 아닌 software.amazon.awssdk를 사용해야 합니다.
-    // key는 저장하고자 하는 파일의 이름(?)을 의미합니다.
+    /**
+     * 파일 업로드를 하기위한 PutObjectRequest를 반환합니다. (software.amazon.awssdk)
+     * key는 저장하고자 하는 파일의 이름
+     *
+     * @param key
+     * @param contentType
+     * @return
+     */
     private PutObjectRequest getPutObjectRequest(String key, String contentType) {
-        log.info("타입: {}", contentType);
         return PutObjectRequest.builder()
                 .bucket(bucketName)
-                .key(key) // TODO invitationCode?
+                .key(key)
                 .contentType(contentType)
                 .build();
     }
 
-    // MultipartFile을 업로드 하기위해 RequestBody.fromInputStream에 InputStream과 file의 Size를 넣어줍니다.
+    /**
+     * MultipartFile을 업로드 하기위해 RequestBody.fromInputStream에 InputStream과 file의 Size를 넣어준다.
+     *
+     * @param file
+     * @return
+     * @throws IOException
+     */
     private RequestBody getFileRequestBody(MultipartFile file) throws IOException {
         return RequestBody.fromInputStream(file.getInputStream(), file.getSize());
     }
 
-    // S3Utilities를 통해 GetUrlRequest를 파라미터로 넣어 파라미터로 넘어온 key의 접근 경로를 URL로 반환받아 경로를 사용할 수 있다.
+    /**
+     * S3Utilities를 통해 GetUrlRequest를 파라미터로 넣어 파라미터로 넘어온 key의 접근 경로를 URL로 반환받아 경로를 사용할 수 있다.
+     *
+     * @param key
+     * @return
+     */
     private String findUploadKeyUrl(String key) {
         GetUrlRequest request = GetUrlRequest.builder()
                 .bucket(bucketName)
@@ -85,8 +115,8 @@ public class S3Util {
         return url.toString();
     }
 
-    public void deleteFile(String filename) {
-        String key = prefix + "/" + filename;
+    public void deleteFile(String prefix, Long id, String filename) {
+        String key = generateKey(prefix, id) + "/" + filename;
         try {
             DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
                     .bucket(bucketName)
