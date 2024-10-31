e16a1caf10afb960870802303be6ad12f6319399
홍창기 <hckhong@naver.com>
2024-10-31 13:19:30 +0900

    feat: #15 사용자 엔티티 구현, 사용자 Repository 구현

---
+++ backend/jaegwan/src/main/java/com/bwmanager/jaegwan/member/entity/Member.java
@@ -0,0 +1,40 @@
+package com.bwmanager.jaegwan.member.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String role;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+}
