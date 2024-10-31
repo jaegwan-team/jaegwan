c06d80428c849364b8717a557444b8ed4c184ae6
KimSunWoong <linehero94@gmail.com>
2024-10-31 18:01:38 +0900

    [FE] feat : 메인페이지 제작 시작

--- frontend/src/app/main/layout.tsx
+++ frontend/src/app/main/layout.tsx
@@ -1,19 +1,23 @@
 import type { Metadata } from "next";
-import "../../styles/globals.css";
+import "../../../styles/globals.css";
+import Header from "@/features/ui/header";
 
 export const metadata: Metadata = {
   title: "재관둥이",
   description: "식당 재고 관리 서비스",
 };
 
-export default function RootLayout({
+export default function MainLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en" className="font-pretendard">
-      <body className="font-pretendard">{children}</body>
+      <body className="font-pretendard">
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ frontend/src/features/ui/header.tsx
@@ -0,0 +1,9 @@
+import styles from "../../../styles/header.module.css";
+
+export default function Header() {
+  return (
+    <header className={styles.layout}>
+      <div className={styles.headerbox}>헤더자리</div>
+    </header>
+  );
+}
