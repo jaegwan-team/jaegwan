4164be9b620a24c4a943f615a42ccef616751a7f
daunjeong824 <wjdek88@naver.com>
2024-11-01 13:57:13 +0900

    feat: Login Layout 뼈대 설계

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,17 +1,20 @@
 "use client";
 
 import videoSrc from "../../../public/video.mp4";
+import LoginLayout from "@/features/ui/layout/loginLayout";
 
 const Login = () => {
     return (
         <div className="flex flex-col justify-between items-center p-24 min-h-screen overflow-hidden">
+            <LoginLayout/>
             <video
                 src={videoSrc}
                 autoPlay
                 muted
                 loop
-                className="absolute top-0 w-full h-full object-cover -z-10"
+                className="absolute top-0 w-full h-full object-cover -z-10 blur-sm brightness-75"
             />
+            
         </div>
     );
 }
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -2,13 +2,16 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
+
+        loginBackground: "#A4D1A5",
+        loginBackgroundTransparent: "rgba(164, 209, 165, 0.45)",
+
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
