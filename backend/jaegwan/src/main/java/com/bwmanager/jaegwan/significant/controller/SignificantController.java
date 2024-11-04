6d0da2f9cc039e509aa044ddc4c245cb07ffa7dc
박세건 <qkrtprjs456@naver.com>
2024-11-04 09:46:40 +0900

    refactor : application.yml 파일 수정으로 인한 리팩토링

--- backend/jaegwan/src/main/java/com/bwmanager/jaegwan/significant/controller/SignificantController.java
+++ backend/jaegwan/src/main/java/com/bwmanager/jaegwan/significant/controller/SignificantController.java
@@ -1,14 +1,34 @@
 package com.bwmanager.jaegwan.significant.controller;
 
+import com.bwmanager.jaegwan.significant.dto.SignificantReadResponse;
 import com.bwmanager.jaegwan.significant.service.SignificantService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/significant")
+@RequestMapping("/significant")
+@Slf4j
 public class SignificantController {
 
-    private SignificantService significantService;
+    private final SignificantService significantService;
+
+    @GetMapping("")
+    ResponseEntity<?> getSignificants() {
+        List<SignificantReadResponse> significantReadResponses = significantService.getSignificants();
+        return ResponseEntity.ok(significantReadResponses);
+    }
+
+    @GetMapping("/{significantId}")
+    ResponseEntity<?> getSignificant(@PathVariable long significantId) {
+        SignificantReadResponse significant = significantService.getSignificant(significantId);
+        return ResponseEntity.ok(significant);
+    }
+
 }
