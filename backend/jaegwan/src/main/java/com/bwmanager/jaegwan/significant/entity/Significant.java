4096158d2cae42972371fa617f1dd209fa758715
박세건 <qkrtprjs456@naver.com>
2024-11-04 11:10:03 +0900

    refactor: 날짜 자동 생성시간 설정 추가

--- backend/jaegwan/src/main/java/com/bwmanager/jaegwan/significant/entity/Significant.java
+++ backend/jaegwan/src/main/java/com/bwmanager/jaegwan/significant/entity/Significant.java
@@ -3,6 +3,7 @@ package com.bwmanager.jaegwan.significant.entity;
 import com.bwmanager.jaegwan.restaurant.entity.Restaurant;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,8 +16,10 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
