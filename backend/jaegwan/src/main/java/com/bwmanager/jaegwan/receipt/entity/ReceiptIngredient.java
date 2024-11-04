d90ea8ace4f3ba9a739f845cb79b069cf3ada8ef
ktk3939 <rkdxorud3939@naver.com>
2024-11-04 15:36:59 +0900

    feat: #26 영수증-재료 엔티티 생성

---
+++ backend/jaegwan/src/main/java/com/bwmanager/jaegwan/receipt/entity/ReceiptIngredient.java
@@ -0,0 +1,44 @@
+package com.bwmanager.jaegwan.receipt.entity;
+
+import com.bwmanager.jaegwan.ingredient.entity.Ingredient;
+import com.bwmanager.jaegwan.restaurant.entity.Restaurant;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class ReceiptIngredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private double amount;
+
+    @Column(nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private boolean isConfirmed;
+
+    @CreatedDate
+    private LocalDateTime creationDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receipt_id", nullable = false)
+    private Receipt receipt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ingredient_id", nullable = false)
+    private Ingredient ingredient;
+
+}
