229fb6eb90acb268e407c73fd3f0be863982969a
ktk3939 <rkdxorud3939@naver.com>
2024-11-05 14:34:06 +0900

    docs: #29 문장 끝에 '.' 추가

--- backend/jaegwan/src/main/java/com/bwmanager/jaegwan/ingredient/controller/IngredientController.java
+++ backend/jaegwan/src/main/java/com/bwmanager/jaegwan/ingredient/controller/IngredientController.java
@@ -18,7 +18,7 @@ public class IngredientController {
     public ResponseEntity<?> getIngredientInfo(@RequestBody IngredientRequest request) {
         CommonResponse<Object> response = CommonResponse.builder()
                 .data(ingredientService.getIngredientsInfo(request.getRestaurantId()))
-                .message("종류별 재료 현황 조회에 성공했습니다")
+                .message("종류별 재료 현황 조회에 성공했습니다.")
                 .build();
 
         return ResponseEntity.ok(response);
@@ -28,7 +28,7 @@ public class IngredientController {
     public ResponseEntity<?> getIngredientInfo(@PathVariable("id") Long id) {
         CommonResponse<Object> response = CommonResponse.builder()
                 .data(ingredientService.getIngredientDetailsInfo(id))
-                .message("한 종류의 재료 상세 조회에 성공했습니다")
+                .message("한 종류의 재료 상세 조회에 성공했습니다.")
                 .build();
 
         return ResponseEntity.ok(response);
