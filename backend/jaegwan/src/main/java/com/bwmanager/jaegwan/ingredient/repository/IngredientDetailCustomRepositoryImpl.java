afb88728f143773f6e1430064e4959b82fd022c0
ktk3939 <rkdxorud3939@naver.com>
2024-11-07 17:00:28 +0900

    refactor: #40 Expressions를 static import

--- backend/jaegwan/src/main/java/com/bwmanager/jaegwan/ingredient/repository/IngredientDetailCustomRepositoryImpl.java
+++ backend/jaegwan/src/main/java/com/bwmanager/jaegwan/ingredient/repository/IngredientDetailCustomRepositoryImpl.java
@@ -4,8 +4,6 @@ import com.bwmanager.jaegwan.ingredient.dto.IngredientDetailResponse;
 import com.bwmanager.jaegwan.ingredient.dto.IngredientResponse;
 import com.bwmanager.jaegwan.ingredient.dto.QIngredientDetailResponse;
 import com.bwmanager.jaegwan.ingredient.dto.QIngredientResponse;
-import com.querydsl.core.types.Projections;
-import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -14,6 +12,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import static com.bwmanager.jaegwan.ingredient.entity.QIngredientDetail.ingredientDetail;
+import static com.querydsl.core.types.dsl.Expressions.numberTemplate;
 
 @RequiredArgsConstructor
 @Repository
@@ -23,16 +22,15 @@ public class IngredientDetailCustomRepositoryImpl implements IngredientDetailCus
 
     @Override
     public IngredientResponse getIngredientInfo(Long ingredientId) {
-
         return jpaQueryFactory
                 .select(new QIngredientResponse(
                         ingredientDetail.ingredient.id,
                         ingredientDetail.ingredient.category,
                         ingredientDetail.amount.sum(),
                         ingredientDetail.ingredient.unit,
-                        Expressions.numberTemplate(Integer.class,
+                        numberTemplate(Integer.class,
                                 "DATEDIFF({0}, {1})",
-                                ingredientDetail.expirationDate.min(), LocalDateTime.now()).as("leftExpirationDay")))
+                                ingredientDetail.expirationDate.min(), LocalDateTime.now())))
                 .from(ingredientDetail)
                 .where(ingredientDetail.ingredient.id.eq(ingredientId))
                 .fetchOne();
@@ -40,14 +38,13 @@ public class IngredientDetailCustomRepositoryImpl implements IngredientDetailCus
 
     @Override
     public List<IngredientDetailResponse> getIngredientDetailsInfoByIngredientId(Long ingredientId) {
-
         return jpaQueryFactory
                 .select(new QIngredientDetailResponse(
                         ingredientDetail.purchaseDate,
                         ingredientDetail.amount,
-                        Expressions.numberTemplate(Integer.class,
+                        numberTemplate(Integer.class,
                                 "DATEDIFF({0}, {1})",
-                                ingredientDetail.expirationDate, LocalDateTime.now()).as("leftExpirationDay")))
+                                ingredientDetail.expirationDate, LocalDateTime.now())))
                 .from(ingredientDetail)
                 .where(ingredientDetail.ingredient.id.eq(ingredientId))
                 .fetch();
