4cf78381e50758b81025a0bafd112aaef0648c7b
KimSunWoong <linehero94@gmail.com>
2024-11-12 12:14:49 +0900

    [FE] 로그인 구현 완료

--- frontend/src/features/ui/layout/kakaologinLayout.tsx
+++ frontend/src/features/ui/layout/kakaologinLayout.tsx
@@ -1,8 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import Image from "next/image";
 import "../../../../styles/globals.css";
 import kaka from "../../../../public/kakao_login_large_narrow.png";
 import lolo from "../../../../public/logo.png";
+import { useRouter } from "next/router";
+
+const BACKEND_BASE_URL = process.env.NEXT_PUBLIC_ADDRESS;
+
 const LoginLayout = () => {
+  const router = useRouter();
+
+  const doLogin = async () => {
+    try {
+      const response = await fetch(`${BACKEND_BASE_URL}/api/auth/kakao/login`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (!response.ok) {
+        router.push("/login");
+      }
+      router.push("/main");
+    } catch (error) {
+      router.push("/login");
+    }
+  };
+
   return (
     <div className="w-2/7 p-4 bg-loginBackgroundTransparent rounded-lg">
       <div className="max-w-sm mx-auto my-10 flex flex-col items-center drop-shadow-2xl">
@@ -11,7 +35,7 @@ const LoginLayout = () => {
         </div>
         <button
           onClick={() => {
-            console.log("카카오 로그인");
+            doLogin();
           }}
           className="w-4/5 transition-transform hover:scale-105"
         >
