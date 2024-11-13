bd0c8880b68af1970ceb2b424ea08070f57886d2
KimSunWoong <linehero94@gmail.com>
2024-11-13 15:09:59 +0900

    [FE] 로그인 5차 수정

--- frontend/src/app/main/template.tsx
+++ frontend/src/app/main/template.tsx
@@ -1,10 +1,15 @@
 "use client";
+
 import Header from "@/features/ui/header";
 import styles from "../../../styles/main.module.css";
 import TabNavigation from "../../features/ui/TabNavigation";
 import UserProvider from "../../features/users/api/login/loginUsers";
 
-export function Providers({ children }: { children: React.ReactNode }) {
+export default function MainTemplate({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <UserProvider>
       <div className={styles.layout}>
