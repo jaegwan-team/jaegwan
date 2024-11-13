78fdfbacaa6a7676259e9233cf47315e7f1bc946
daunjeong824 <wjdek88@naver.com>
2024-11-13 09:55:33 +0900

    refactor: api 연동 테스트 (구매내역 조회 - 4-1)

--- frontend/src/app/main/page.tsx
+++ frontend/src/app/main/page.tsx
@@ -101,14 +101,14 @@ export default function MainPage() {
   };
 
   /* <임시> 구매 내역 API */
-  const getCookie = (name: string): string | undefined => {
-    const value = `; ${document.cookie}`;
-    console.log("=== cookie value ===")
-    console.log(value)
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop()?.split(';').shift();
-    return undefined;
-  }
+  // const getCookie = (name: string): string | undefined => {
+  //   const value = `; ${document.cookie}`;
+  //   console.log("=== cookie value ===")
+  //   console.log(value)
+  //   const parts = value.split(`; ${name}=`);
+  //   if (parts.length === 2) return parts.pop()?.split(';').shift();
+  //   return undefined;
+  // }
 
   const fetchIngredientList = async () => {
     // const token = getCookie('accessToken');
